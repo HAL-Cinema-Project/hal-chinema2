984b4c9b5c7854f3c773db0239b45e4ed62f96c6
taku10101 <taku1011ht@gmail.com>
2024-12-07 16:53:53 +0900

    movie一覧追加

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -1,3 +1,58 @@
-export const Page = () => {
-  return <div>page</div>;
+import Link from "next/link";
+import { MoviesMock } from "../../../mock/types/movies";
+
+interface MoviePropsInterface {
+  movies: MoviesMock[];
+}
+
+export const Page = (props: MoviePropsInterface) => {
+  const { movies } = props;
+  return (
+    <div style={{ padding: "20px" }}>
+      {movies &&
+        movies.map((item: MoviesMock) => (
+          <div
+            key={item.id}
+            style={{
+              marginBottom: "20px",
+              border: "1px solid #ccc",
+              padding: "10px",
+              borderRadius: "5px",
+            }}
+          >
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <div style={{ marginRight: "20px" }}>
+                <img
+                  src={item.thumbnail}
+                  alt='Movie Poster'
+                  style={{ width: "100px", height: "150px" }}
+                />
+              </div>
+              <div>
+                <div style={{ marginBottom: "10px" }}>
+                  <b>{item.movieName}</b>
+                </div>
+                <div style={{ marginBottom: "10px" }}>
+                  <span>監督名：</span>
+                  <span>{item.director}</span>
+                </div>
+                <Link href={`${item.link}`}>
+                  <button
+                    style={{
+                      padding: "5px 10px",
+                      backgroundColor: "#555",
+                      color: "#fff",
+                      border: "none",
+                      borderRadius: "5px",
+                    }}
+                  >
+                    詳細へ
+                  </button>
+                </Link>
+              </div>
+            </div>
+          </div>
+        ))}
+    </div>
+  );
 };
--- src/app/schedules/page.tsx
+++ src/app/schedules/page.tsx
@@ -1,3 +1,40 @@
+import { useMovies } from "../../../mock/hooks/useMovies";
+import { useSchedules } from "../../../mock/hooks/useSchedule";
+import { ScheduleMock } from "../../../mock/types";
+import { MoviesMock } from "../../../mock/types/movies";
+
 export const Page = () => {
-  return <div>page</div>;
+  const { movies } = useMovies();
+  const { schedules } = useSchedules();
+  const scheduleList = schedules.map((schedule: ScheduleMock) => {
+    const movie = movies.find(
+      (movie: MoviesMock) => Number(movie.id) === Number(schedule.movieId)
+    );
+    console.log("Found Movie:", movie);
+    return {
+      ...schedule,
+      movieName: movie ? movie.movieName : "",
+    };
+  });
+
+  console.log(scheduleList);
+
+  return (
+    <div style={{ padding: "20px" }}>
+      {scheduleList.map((schedule: ScheduleMock, index) => (
+        <div key={index} style={{ marginBottom: "20px" }}>
+          <p
+            style={{
+              fontSize: "1.2rem",
+              fontWeight: "bold",
+              margin: "10px 0",
+              color: "#fff",
+            }}
+          >
+            {schedule.theater} {schedule.movieName}
+          </p>
+        </div>
+      ))}
+    </div>
+  );
 };
