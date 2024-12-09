721d9b8c95528f7bf9b5940c4d4a1e7e0f22e2ee
taku10101 <taku1011ht@gmail.com>
2024-12-09 09:49:49 +0900

    いらないインポート消した

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { useMovies } from "../../../mock/hooks/useMovies";
-import { MoviesMock } from "../../../mock/types/movies";
 import Link from "next/link";
 
 const Page = () => {
