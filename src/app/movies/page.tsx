e3635e16498c4011d8c9667c48300210a4127fe4
10yu30 <yuu2003151030@gmail.com>
2024-12-16 10:16:18 +0900

    add movies create

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -2,61 +2,71 @@
 import { useMovies } from "../../../mock/hooks/useMovies";
 import Link from "next/link";
 import { MoviesMock } from "../../../mock/types/movies";
+import { Box, Flex, Button, Image, Text } from "@yamada-ui/react";
 
-const Page = () => {
+export const Page = () => {
   const { movies } = useMovies();
 
   if (!Array.isArray(movies)) {
-    return <div>映画データが正しく読み込まれていません。</div>;
+    return <Box>映画データが正しく読み込まれていません。</Box>;
   }
 
   return (
-    <div style={{ padding: "20px", width: "100%" }}>
-      {movies &&
-        movies.map((item: MoviesMock) => (
-          <div
-            key={item.id}
-            style={{
-              marginBottom: "20px",
-              border: "1px solid #ccc",
-              padding: "10px",
-              borderRadius: "5px",
-            }}
-          >
-            <div style={{ display: "flex", alignItems: "center" }}>
-              <div style={{ marginRight: "20px" }}>
-                <img
-                  src={item.thumbnail}
-                  alt='Movie Poster'
-                  style={{ width: "100px", height: "150px" }}
-                />
-              </div>
-              <div>
-                <div style={{ marginBottom: "10px" }}>
-                  <b>{item.movieName}</b>
-                </div>
-                <div style={{ marginBottom: "10px" }}>
-                  <span>監督名：</span>
-                  <span>{item.director}</span>
-                </div>
-                <Link href={`${item.link}`}>
-                  <button
-                    style={{
-                      padding: "5px 10px",
-                      backgroundColor: "#555",
-                      color: "#fff",
-                      border: "none",
-                      borderRadius: "5px",
-                    }}
-                  >
-                    詳細へ
-                  </button>
-                </Link>
-              </div>
-            </div>
-          </div>
+    <Box p="20px" w="100%" maxW="1000px" mx="auto">
+      <Flex wrap="wrap" gap="20px" justify="flex-start">
+        {movies.map((item: MoviesMock) => (
+          <Box
+          key={item.id}
+          width="302px"
+          borderWidth="1px"
+          borderColor="gray.200"
+          p="10px"
+          borderRadius="md"
+          boxSizing="border-box"
+          display="flex"
+          flexDirection="column"
+          justifyContent="space-between"
+        >
+          <Box>
+            <Image
+              src={item.movieImage?.[0] || "/placeholder.png"}
+              alt={item.movieName || "映画タイトル"}
+              width="280px"
+              height="400px"
+              objectFit="cover"
+              borderRadius="md"
+              mb="10px"
+            />
+            <Text mb="10px" fontWeight="bold" textAlign="center">
+              {item.movieName || "映画タイトル"}
+            </Text>
+            <Text mb="10px" textAlign="center">
+              <span>監督名：</span>
+              <span>{item.director || "不明"}</span>
+            </Text>
+          </Box>
+          <Flex justify="center" mt="auto">
+            <Link href={item.link || "#"} passHref>
+              <Button
+                as="a"
+                w="200"
+                bg="gray.600"
+                color="white"
+                _hover={{
+                  bg: "#bbb",
+                }}
+                borderRadius="md"
+                disabled={!item.link}
+              >
+                詳細へ
+              </Button>
+            </Link>
+          </Flex>
+        </Box>
+        
         ))}
-    </div>
+      </Flex>
+    </Box>
   );
 };
 
