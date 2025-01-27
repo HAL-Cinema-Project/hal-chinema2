346cd2ed68d82ad636e61432974b87b9e493b76b
xBlackTea <xBlackTea.Official@gmail.com>
2025-01-27 10:49:36 +0900

    add: header色追加 遷移追加

--- src/layouts/siteLayout/section/Header.tsx
+++ src/layouts/siteLayout/section/Header.tsx
@@ -1,25 +1,29 @@
 import { Box, Image } from "@yamada-ui/react"
+import Link from "next/link"
 
 export const Header = () => {
   return (
     <Box
-        display="flex"
-        justifyContent="space-between"
-        alignItems="center"
-        width="100%"
-        height="50px"
-        padding="0 15px"
-        bgColor="#1e2a3a"
+      display="flex"
+      justifyContent="space-between"
+      alignItems="center"
+      width="100%"
+      height="50px"
+      padding="0 15px"
+      bgColor="#121922"
     >
+      <Link href="http://localhost:3000">
         <Image
-            display="flex"
-            justifyContent="center"
-            alignItems="center"
-            maxH="60%"
-            src="/HAL_Cinema_Logo_White.svg"
-            objectFit="cover"
-            alt="HAL_Cinema_Logo_White"
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          height="30px"
+          src="/HAL_Cinema_Logo_White.svg"
+          objectFit="cover"
+          alt="HAL_Cinema_Logo_White"
+          cursor="pointer"
         />
+      </Link>
     </Box>
   )
 }
