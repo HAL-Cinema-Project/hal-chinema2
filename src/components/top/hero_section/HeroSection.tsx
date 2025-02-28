ee5b4128503c5f1018f6712080d0a0b35f4a6b23
dango-the-world <z.rohaso@gmail.com>
2025-02-28 14:27:26 +0900

    add: ヒーローセクションに画像を追加し、カルーセルを自動再生に設定

--- src/components/top/hero_section/HeroSection.tsx
+++ src/components/top/hero_section/HeroSection.tsx
@@ -1,19 +1,27 @@
-import { Box, Center } from "@yamada-ui/react";
+import { Box, Center, Image } from "@yamada-ui/react";
 import { Carousel, CarouselSlide } from "@yamada-ui/carousel";
 
 const HeroSection = () => {
   return (
-    <Box display={"flex"} justifyContent={"center"}>
-      <Carousel withControls={false} w={"99vw"} h={"100vh"}>
-        <CarouselSlide bg="primary">1</CarouselSlide>
-        <CarouselSlide as={Center} bg="secondary">
-          2
+    <Box display="flex" justifyContent="center">
+      <Carousel withControls={false} w="99vw" h="100vh" autoplay>
+        <CarouselSlide as={Center} overflow="hidden">
+          <Image
+            alt=""
+            src="image/IMG_1801.png"
+            w="100%"
+            h="100%"
+            objectFit="cover"
+          />
         </CarouselSlide>
-        <CarouselSlide as={Center} bg="warning">
-          3
-        </CarouselSlide>
-        <CarouselSlide as={Center} bg="danger">
-          4
+        <CarouselSlide as={Center} overflow="hidden">
+          <Image
+            alt=""
+            src="image/IMG_1802.jpg"
+            w="100%"
+            h="100%"
+            objectFit="cover"
+          />
         </CarouselSlide>
       </Carousel>
     </Box>
